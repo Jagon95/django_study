0fa38e8f07408916da5295f54c631462bc586aff
Jagon95 <jagon95@gmail.com>
2016-11-12 17:42:25 +0300

    Do ex. 5. Add some tests

---
+++ polls/strings.py
@@ -0,0 +1,5 @@
+"""It's like strings.xml in android projects."""
+
+latest_question_list = 'latest_question_list'
+no_polls_available = 'No polls are available.'
+past_question = 'Past question.'
--- polls/views.py
+++ polls/views.py
@@ -6,25 +6,35 @@ from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .models import Question, Choice
 from django.views import generic
+from django.utils import timezone
+from .strings import *
 
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
+    context_object_name = latest_question_list
 
     def get_queryset(self):
-        """Return the last five published question"""
-        return Question.objects.order_by('-pub_date')[:5]
+        """Return the last five published question. not including those set to be published in the future)."""
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        """Excludes any questions that aren't published yet."""
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
 
 class ResultView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
+
+    def get_queryset(self):
+        """Excludes any questions that aren't published yet."""
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 
 def vote(request, question_id):
